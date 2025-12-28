90e6eefb173efd077e513cb7073deeb3e0d78090
Chris25005 <chriswakar@gamil.com>
2025-12-28 14:15:44 +0530

    added diployed URL

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-netlify-app.netlify.app"
+      "https://spontaneous-cuchufli-8e6709.netlify.app/"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
