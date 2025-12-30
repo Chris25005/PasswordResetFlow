054de2177cea1e2ebf6e57a3109885d958010d8f
Chris25005 <chriswakar@gamil.com>
2025-12-30 21:56:57 +0530

    deployed link added

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://spontaneous-cuchufli-8e6709.netlify.app"
+      "https://comfy-manatee-a5f0fa.netlify.app/"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
